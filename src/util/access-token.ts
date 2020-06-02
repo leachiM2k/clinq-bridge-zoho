1a81805c47693d48c23840b04edc6df9cb6eee16
Michael Rotmanov <leachiM2k@leachiM2k.de>
2020-06-02 08:48:45 +0200

    migrated 'request' to 'axios'

--- src/util/access-token.ts
+++ src/util/access-token.ts
@@ -1,33 +1,50 @@
-import { IZohoAuthResponse, RequestMethods } from './interfaces';
-import { makeRequest } from './make-request';
-import parseEnvironment from './parse-environment';
+import axios from "axios";
+import querystring from "querystring";
+import { IZohoAuthResponse } from "./interfaces";
+import parseEnvironment from "./parse-environment";
 
-export async function authorizeApiKey(apiKey: string, apiUrl: string): Promise<{ accessToken: string, apiDomain: string }> {
-    if (typeof apiKey !== "string" || !apiKey || apiKey.trim().length === 0) {
-        throw new Error("Invalid API key.");
-    }
-    const [, refreshToken] = apiKey.split(":");
+export async function authorizeApiKey(
+  apiKey: string,
+  apiUrl: string
+): Promise<{ accessToken: string; apiDomain: string }> {
+  if (typeof apiKey !== "string" || !apiKey || apiKey.trim().length === 0) {
+    throw new Error("Invalid API key.");
+  }
+  const [, refreshToken] = apiKey.split(":");
 
-    if (!refreshToken) {
-        throw new Error('Could not extract refresh token from api key');
-    }
+  if (!refreshToken) {
+    throw new Error("Could not extract refresh token from api key");
+  }
 
-    const { access_token, api_domain } = await getNewAccessToken(refreshToken, apiUrl);
-    // TODO: maybe it is worth to cache the access token? (with the refresher token as key)
-    return { accessToken: access_token, apiDomain: api_domain };
+  const { access_token, api_domain } = await getNewAccessToken(
+    refreshToken,
+    apiUrl
+  );
+  // TODO: maybe it is worth to cache the access token? (with the refresher token as key)
+  return { accessToken: access_token, apiDomain: api_domain };
 }
 
-function getNewAccessToken(refreshToken: string, accountServer: string): Promise<IZohoAuthResponse> {
-    const { ZOHO_CLIENT_ID, ZOHO_CLIENT_SECRET } = parseEnvironment();
-    const reqOptions = {
-        url: `${accountServer}/oauth/v2/token`,
-        method: RequestMethods.POST,
-        qs: {
-            refresh_token: refreshToken,
-            client_id: ZOHO_CLIENT_ID,
-            client_secret: ZOHO_CLIENT_SECRET,
-            grant_type: 'refresh_token'
-        }
-    };
-    return makeRequest(reqOptions) as Promise<IZohoAuthResponse>;
+async function getNewAccessToken(
+  refreshToken: string,
+  accountServer: string
+): Promise<IZohoAuthResponse> {
+  const { ZOHO_CLIENT_ID, ZOHO_CLIENT_SECRET } = parseEnvironment();
+
+  const payload = {
+    refresh_token: refreshToken,
+    client_id: ZOHO_CLIENT_ID,
+    client_secret: ZOHO_CLIENT_SECRET,
+    grant_type: "refresh_token"
+  };
+
+  const response = await axios.post<IZohoAuthResponse>(
+    `${accountServer}/oauth/v2/token?${querystring.encode(payload)}`,
+    {}
+  );
+
+  if (!response || response.status !== 200) {
+    return Promise.reject(`Error in Zoho response: ${response.statusText}`);
+  }
+
+  return response.data;
 }
--- src/util/zoho.ts
+++ src/util/zoho.ts
@@ -1,129 +1,200 @@
-import { Contact, ContactTemplate, ContactUpdate } from '@clinq/bridge';
-import querystring from 'querystring';
-import { authorizeApiKey } from './access-token';
-import { convertContactToZohoContact, convertZohoContactToContact } from './contact';
-import { IZohoAuthResponse, IZohoContactsResponse, RequestMethods } from './interfaces';
-import { isZohoContactResponse, isZohoUpdateResponse, makeRequest } from './make-request';
-import parseEnvironment from './parse-environment';
+import { Contact, ContactTemplate, ContactUpdate } from "@clinq/bridge";
+import axios from "axios";
+import querystring from "querystring";
+import { authorizeApiKey } from "./access-token";
+import {
+  convertContactToZohoContact,
+  convertZohoContactToContact
+} from "./contact";
+import {
+  isZohoContactResponse,
+  isZohoUpdateResponse,
+  IZohoAuthResponse,
+  IZohoContactsResponse,
+  IZohoUpdateResponse,
+  RequestMethods
+} from "./interfaces";
+import parseEnvironment from "./parse-environment";
+
+const ZOHO_OAUTH_TOKEN_NAME = "Zoho-oauthtoken";
 
 export async function upsertZohoContact(
-    apiKey: string,
-    apiUrl: string,
-    contact: ContactUpdate | ContactTemplate
+  apiKey: string,
+  apiUrl: string,
+  contact: ContactUpdate | ContactTemplate
 ): Promise<Contact> {
-    const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
-    const id = (contact as ContactUpdate).id;
-    const zohoContact = convertContactToZohoContact(contact, id);
+  const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
+  const id = (contact as ContactUpdate).id;
+  const zohoContact = convertContactToZohoContact(contact, id);
 
-    const reqOptions = {
-        url: `${apiDomain}/crm/v2/Contacts`,
-        method: id ? RequestMethods.PUT : RequestMethods.POST,
-        body: { data: [zohoContact] }
-    };
-    const response = await makeRequest(reqOptions, accessToken);
+  const responseRequest = await axios.request<IZohoContactsResponse>({
+    url: `${apiDomain}/crm/v2/Contacts`,
+    method: id ? RequestMethods.PUT : RequestMethods.POST,
+    data: { data: [zohoContact] },
+    headers: { Authorization: `${ZOHO_OAUTH_TOKEN_NAME} ${accessToken}` }
+  });
 
-    if (!response || !isZohoUpdateResponse(response) || response.data.length !== 1) {
-        throw new Error("Received unexpected response from Zoho");
-    }
+  if (!responseRequest || responseRequest.status >= 300) {
+    return Promise.reject(
+      `Error in Zoho response: ${responseRequest.statusText}`
+    );
+  }
 
-    const [updateEntry] = response.data;
-    if (updateEntry.code !== 'SUCCESS') {
-        throw new Error(`Could not upsert Zoho contact: ${updateEntry.message}`);
-    }
+  const response = responseRequest.data;
 
-    const reqGetOptions = {
-        url: `${apiDomain}/crm/v2/Contacts/${updateEntry.details.id}`,
-        method: RequestMethods.GET,
-    };
-    const responseGet = await makeRequest(reqGetOptions, accessToken);
+  if (
+    !response ||
+    !isZohoUpdateResponse(response) ||
+    response.data.length !== 1
+  ) {
+    throw new Error("Received unexpected response from Zoho");
+  }
 
-    const receivedContact = convertZohoContactToContact((responseGet as IZohoContactsResponse).data[ 0 ]);
-    if (!receivedContact) {
-        throw new Error("Could not parse received contact");
-    }
-    return receivedContact;
+  const [updateEntry] = response.data;
+  if (updateEntry.code !== "SUCCESS") {
+    throw new Error(`Could not upsert Zoho contact: ${updateEntry.message}`);
+  }
+
+  const responseGet = await axios.get<IZohoContactsResponse>(
+    `${apiDomain}/crm/v2/Contacts/${updateEntry.details.id}`,
+    { headers: { Authorization: `${ZOHO_OAUTH_TOKEN_NAME} ${accessToken}` } }
+  );
+
+  if (!responseGet || responseGet.status !== 200) {
+    return Promise.reject(`Error in Zoho response: ${responseGet.statusText}`);
+  }
+
+  const receivedContact = convertZohoContactToContact(
+    (responseGet.data as IZohoContactsResponse).data[0]
+  );
+  if (!receivedContact) {
+    throw new Error("Could not parse received contact");
+  }
+  return receivedContact;
 }
 
 export async function getZohoContacts(
-    apiKey: string,
-    apiUrl: string
+  apiKey: string,
+  apiUrl: string
 ): Promise<Contact[]> {
-    const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
-    return getPaginatedZohoContacts(accessToken, apiDomain);
+  const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
+  return getPaginatedZohoContacts(accessToken, apiDomain);
 }
 
 async function getPaginatedZohoContacts(
-    accessToken: string,
-    apiDomain: string,
-    page: number = 1,
-    previousContacts?: Contact[]
+  accessToken: string,
+  apiDomain: string,
+  page: number = 1,
+  previousContacts?: Contact[]
 ): Promise<Contact[]> {
-    const reqOptions = { url: `${apiDomain}/crm/v2/Contacts`, method: RequestMethods.GET, qs: { page } };
-    const response = await makeRequest(reqOptions, accessToken);
+  const responseGet = await axios.get<IZohoContactsResponse>(
+    `${apiDomain}/crm/v2/Contacts?page=${page}`,
+    { headers: { Authorization: `${ZOHO_OAUTH_TOKEN_NAME} ${accessToken}` } }
+  );
 
-    if (!isZohoContactResponse(response)) {
-        return [];
+  if (!responseGet || responseGet.status !== 200) {
+    return Promise.reject(`Error in Zoho response: ${responseGet.statusText}`);
+  }
+
+  const response = responseGet.data;
+
+  if (!isZohoContactResponse(response)) {
+    return [];
+  }
+
+  const contacts: Contact[] = previousContacts || [];
+
+  for (const zohoContact of response.data) {
+    const contact = convertZohoContactToContact(zohoContact);
+
+    if (contact) {
+      contacts.push(contact);
     }
+  }
 
-    const contacts: Contact[] = previousContacts || [];
+  if (response.info.more_records) {
+    return getPaginatedZohoContacts(accessToken, apiDomain, page + 1, contacts);
+  }
 
-    for (const zohoContact of response.data) {
-        const contact = convertZohoContactToContact(zohoContact);
-
-        if (contact) {
-            contacts.push(contact);
-        }
-    }
-
-    if (response.info.more_records) {
-        return getPaginatedZohoContacts(accessToken, apiDomain, page + 1, contacts);
-    }
-
-    return contacts;
+  return contacts;
 }
 
-export async function deleteZohoContact(apiKey: string, apiUrl: string, id: string): Promise<void> {
-    const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
-    const reqDeleteOptions = {
-        url: `${apiDomain}/crm/v2/Contacts/${id}`,
-        method: RequestMethods.DELETE,
-    };
-    const response = await makeRequest(reqDeleteOptions, accessToken);
+export async function deleteZohoContact(
+  apiKey: string,
+  apiUrl: string,
+  id: string
+): Promise<void> {
+  const { accessToken, apiDomain } = await authorizeApiKey(apiKey, apiUrl);
 
-    if (!response || !isZohoUpdateResponse(response) || response.data.length !== 1) {
-        throw new Error("Received unexpected response from Zoho");
-    }
+  const responseDelete = await axios.delete<IZohoUpdateResponse>(
+    `${apiDomain}/crm/v2/Contacts/${id}`,
+    { headers: { Authorization: `${ZOHO_OAUTH_TOKEN_NAME} ${accessToken}` } }
+  );
 
-    const [updateEntry] = response.data;
-    if (updateEntry.code !== 'SUCCESS') {
-        throw new Error(`Could not delete Zoho contact: ${updateEntry.code} / ${updateEntry.message}`);
-    }
+  if (!responseDelete || responseDelete.status >= 300) {
+    return Promise.reject(
+      `Error in Zoho response: ${responseDelete.statusText}`
+    );
+  }
+
+  const response = responseDelete.data;
+
+  if (
+    !response ||
+    !isZohoUpdateResponse(response) ||
+    response.data.length !== 1
+  ) {
+    throw new Error("Received unexpected response from Zoho");
+  }
+
+  const [updateEntry] = response.data;
+  if (updateEntry.code !== "SUCCESS") {
+    throw new Error(
+      `Could not delete Zoho contact: ${updateEntry.code} / ${updateEntry.message}`
+    );
+  }
 }
 
-export function getTokens(code: string, accountServer: string): Promise<IZohoAuthResponse> {
-    const { ZOHO_CLIENT_ID, ZOHO_CLIENT_SECRET, ZOHO_REDIRECT_URL } = parseEnvironment();
-    const reqOptions = {
-        url: `${accountServer}/oauth/v2/token`,
-        method: RequestMethods.POST,
-        qs: {
-            code,
-            redirect_uri: ZOHO_REDIRECT_URL,
-            client_id: ZOHO_CLIENT_ID,
-            client_secret: ZOHO_CLIENT_SECRET,
-            grant_type: 'authorization_code'
-        }
-    };
-    return makeRequest(reqOptions) as Promise<IZohoAuthResponse>;
+export async function getTokens(
+  code: string,
+  accountServer: string
+): Promise<IZohoAuthResponse> {
+  const {
+    ZOHO_CLIENT_ID,
+    ZOHO_CLIENT_SECRET,
+    ZOHO_REDIRECT_URL
+  } = parseEnvironment();
+
+  const payload = {
+    code,
+    redirect_uri: ZOHO_REDIRECT_URL,
+    client_id: ZOHO_CLIENT_ID,
+    client_secret: ZOHO_CLIENT_SECRET,
+    grant_type: "authorization_code"
+  };
+
+  const response = await axios.post<IZohoAuthResponse>(
+    `${accountServer}/oauth/v2/token?${querystring.encode(payload)}`,
+    {}
+  );
+
+  if (!response || response.status !== 200) {
+    return Promise.reject(`Error in Zoho response: ${response.statusText}`);
+  }
+
+  return response.data;
 }
 
 export function getOAuth2RedirectUrl(): string {
-    const { ZOHO_CLIENT_ID, ZOHO_REDIRECT_URL } = parseEnvironment();
-    return 'https://accounts.zoho.eu/oauth/v2/auth?' + querystring.encode({
-            scope: 'ZohoCRM.modules.contacts.ALL',
-            client_id: ZOHO_CLIENT_ID,
-            response_type: 'code',
-            access_type: 'offline',
-            redirect_uri: ZOHO_REDIRECT_URL
-        }
-    );
+  const { ZOHO_CLIENT_ID, ZOHO_REDIRECT_URL } = parseEnvironment();
+  return (
+    "https://accounts.zoho.eu/oauth/v2/auth?" +
+    querystring.encode({
+      scope: "ZohoCRM.modules.contacts.ALL",
+      client_id: ZOHO_CLIENT_ID,
+      response_type: "code",
+      access_type: "offline",
+      redirect_uri: ZOHO_REDIRECT_URL
+    })
+  );
 }
